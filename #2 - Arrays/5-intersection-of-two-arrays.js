25bcf0821ff0e149f63e69742c41d5e40c8f68f0
Venkatsh-Koneti <konetivenkatsh157@gmail.com>
2025-07-10 23:41:27 +0530

    day 7 problems

---
+++ #2 - Arrays/5-intersection-of-two-arrays.js
@@ -0,0 +1,31 @@
+/*
+    349. Intersection of Two Arrays
+
+    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result 
+    must be unique and you may return the result in any order.
+
+    Example 1:
+        Input: nums1 = [1,2,2,1], nums2 = [2,2]
+        Output: [2]
+
+    Example 2:
+        Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+        Output: [9,4]
+        Explanation: [4,9] is also accepted.
+*/
+
+var intersection = function(nums1, nums2) {
+    const uniqueElementsSet = new Set(nums1);
+    const intersectionSet = new Set();
+
+    for (let element of nums2) {
+        if (uniqueElementsSet.has(element)) {
+            intersectionSet.add(element);
+        }
+    }
+
+    return Array.from(intersectionSet);
+};
+
+const result = intersection([4,9,5], [9,4,9,8,4])
+console.log(result);
---
+++ #2 - Arrays/6-rotate-array.js
@@ -0,0 +1,48 @@
+/*
+    189. Rotate Array
+    Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+    Example 1:
+        Input: nums = [1,2,3,4,5,6,7], k = 3
+        Output: [5,6,7,1,2,3,4]
+        Explanation:
+        rotate 1 steps to the right: [7,1,2,3,4,5,6]
+        rotate 2 steps to the right: [6,7,1,2,3,4,5]
+        rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+    Example 2:
+        Input: nums = [-1,-100,3,99], k = 2
+        Output: [3,99,-1,-100]
+        Explanation: 
+        rotate 1 steps to the right: [99,-1,-100,3]
+        rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+var rotate = function (nums, k) {
+    // for (let i = 0; i < k; i++) {
+    //     const secondEle = nums.pop();
+    //     nums.unshift(secondEle);
+    // }
+    
+    // nums.unshift(...nums.splice((k % nums.length) * -1));
+
+
+    function reverse(arr, start, end) {
+        while (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]];
+        start++;
+        end--;
+        }
+    }
+
+    k %= nums.length;
+
+    reverse(nums, 0, (nums.length - 1));
+    reverse(nums, 0, (k - 1));
+    reverse(nums, k, (nums.length - 1));
+
+    return nums;
+};
+
+const result = rotate([1, 2, 3, 4, 5, 6, 7], 3)
+console.log(result);
