44d947aac039e7a434587b527896b68fc8db34f1
Venkatesh-Koneti <konetivenkatesh157@gmail.com>
2025-07-14 23:39:30 +0530

    completed the missing-numbers problem

---
+++ #2 - Arrays/9-missing-number.js
@@ -0,0 +1,51 @@
+/*
+
+    268. Missing Number
+
+    Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is 
+    missing from the array.
+
+    Example 1:
+        Input: nums = [3,0,1]
+        Output: 2
+        Explanation: n = 3 since there are 3 numbers, so all numbers are in the range [0,3]. 2 is the missing number in the 
+                    range since it does not appear in nums.
+
+    Example 2:
+        Input: nums = [0,1]
+        Output: 2
+        Explanation: n = 2 since there are 2 numbers, so all numbers are in the range [0,2]. 2 is the missing number 
+                    in the range since it does not appear in nums.
+
+    Example 3:
+        Input: nums = [9,6,4,2,3,5,7,0,1]
+        Output: 8
+        Explanation: n = 9 since there are 9 numbers, so all numbers are in the range [0,9]. 8 is the missing 
+                    number in the range since it does not appear in nums.
+
+*/
+
+var missingNumber = function(nums) {
+    let actualSum = 0;
+    // let expectedSum = 0;
+
+    for (let element of nums) {
+        actualSum += element;
+    }
+
+    // for(let i = 0; i < nums.length + 1; i++){
+    //     expectedSum+=i
+    // }
+
+    const totalCount = nums.length;
+    const expectedSum = (totalCount * (totalCount + 1)) / 2;
+    
+    return expectedSum - actualSum;
+
+    // const numsSet = new Set(nums);
+    // for (let i = 0; i <= nums.length + 1; i++) {
+    //     if (!numsSet.has(i)) {
+    //         return i;
+    //     }
+    // }
+};
