762322c4ed3337bbba2666d749a9bd94faacecf5
Venkatesh-Koneti <konetivenkatesh157@gmail.com>
2025-07-25 12:00:00 +0530

    Max Consecutive Ones problem statement

--- #2 - Arrays/14-max-consecutive-ones.js
+++ #2 - Arrays/14-max-consecutive-ones.js
@@ -1,5 +1,5 @@
 /*
-    Given a binary array nums, return the maximum number of consecutive 1's in the array.
+    Given a binary array nums, return the maximum number of consecutive 1's in the array. 
 
     Example 1:
         Input: nums = [1,1,0,1,1,1]
