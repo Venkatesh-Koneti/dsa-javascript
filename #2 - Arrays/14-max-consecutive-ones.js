33a317edd2f101da57d53d394f42721a2b3f3e5b
Venkatesh-Koneti <konetivenkatesh157@gmail.com>
2025-07-26 00:44:37 +0530

    Max Consecutive Ones

--- #2 - Arrays/14-max-consecutive-ones.js
+++ #2 - Arrays/14-max-consecutive-ones.js
@@ -11,3 +11,20 @@
         Output: 2
 */
 
+var findMaxConsecutiveOnes = function(nums) {
+    let maxCount = 0;
+    let currentCount = 0;
+
+    for (let num of nums) {
+        if (num === 1) {
+            currentCount += 1;
+        } else {
+            maxCount = Math.max(maxCount, currentCount);
+            currentCount = 0;
+        }
+    }
+
+    return Math.max(maxCount, currentCount);
+};
+
+console.log(findMaxConsecutiveOnes([1,0,1,1,0,1]));
