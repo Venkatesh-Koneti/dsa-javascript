6b571d418be964980ea926b37a47f7d86e2db200
Venkatesh <konetivenkatesh157@gmail.com>
2025-07-31 00:27:04 +0530

    practied recursion

--- #3 - Recursion/1-sum-of-first-n-numbers.js
+++ #3 - Recursion/1-sum-of-first-n-numbers.js
@@ -1,3 +1,12 @@
+/**
+ * Recursively calculates the sum of first 'n' natural numbers.
+ *
+ * @param {number} n - A non-negative integer.
+ * @returns {number} - The sum of numbers from 1 to n.
+ *
+ * @example
+ * sumOfnNumbers(5); // returns 15
+ */
 function sumOfnNumbers(n) {
     if (n === 0) return 0;
     return n + sumOfnNumbers(n - 1);
--- #3 - Recursion/2-sum-of-all-numbers-in-array.js
+++ #3 - Recursion/2-sum-of-all-numbers-in-array.js
@@ -1,7 +1,17 @@
-function sumOfArray(listOfNumbers, index = 0) {
-    if (index === listOfNumbers.length) return 0;
-    return listOfNumbers[index] + sumOfArray(listOfNumbers, index + 1);
+/**
+ * Recursively calculates the sum of all numbers in an array.
+ *
+ * @param {number[]} numbers - The array of numbers to be summed.
+ * @param {number} [currentIndex=0] - The index of the current element being added.
+ * @returns {number} - The sum of all numbers in the array.
+ *
+ * @example
+ * calculateArraySum([1, 2, 3, 4, 5]); // returns 15
+ */
+function calculateArraySum(numbers, currentIndex = 0) {
+    if (currentIndex === numbers.length) return 0;
+    return numbers[currentIndex] + calculateArraySum(numbers, currentIndex + 1);
 }
 
-const result = sumOfArray([1, 2, 3, 4, 5]);
-console.log(result); // Output: 15
+const result = calculateArraySum([1, 2, 3, 4, 5]);
+console.log(result); // 15
